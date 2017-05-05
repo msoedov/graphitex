6f85e56c63970648a3398ee4dd51b943810e873f
Alex Myasoedov <msoedov@gmail.com>
2017-05-05 19:20:32 -0400

    @Spec for client api

--- lib/graphitex/client.ex
+++ lib/graphitex/client.ex
@@ -15,19 +15,24 @@ defmodule Graphitex.Client do
   @doc """
   Add a node and the service it provides to the directory.
   """
+  @spec metric(number, [String.t]) :: nil
   def metric(value, namespace) when is_list(namespace) do
     metric({value, Enum.join(namespace, ".")})
   end
+  @spec metric(number, binary) :: nil
   def metric(value, namespace) when is_binary(namespace) do
     metric({value, namespace})
   end
+  @spec metric(number, binary|String.t, Float.t) :: nil
   def metric(value, namespace, ts) when is_float(ts) do
     metric({value, namespace, Float.round(ts, 1)})
   end
+  @spec metric({number, binary|String.t, Float.t}) :: nil
   def metric(measurement) do
     GenServer.cast(@name, {:metric, pack_msg(measurement)})
   end
 
+  @spec metric_batch([{number, binary|String.t, Float.t}]) :: nil
   def metric_batch(batch) do
     bulk_mgs = batch
     |> Enum.map(&pack_msg/1)
@@ -39,12 +44,15 @@ defmodule Graphitex.Client do
   # Private API
   #
 
+  @spec pack_msg({number, String.t}) :: binary
   defp pack_msg({val, ns} = _arg) do
     pack_msg(val, ns, :os.system_time(:seconds))
   end
+  @spec pack_msg({number, String.t, Float.t}) :: binary
   defp pack_msg({val, ns, ts} = _arg) do
     pack_msg(val, ns, ts)
   end
+  @spec pack_msg(number, String.t, Float.t) :: binary
   defp pack_msg(val, ns, ts) do
     '#{ns} #{val} #{ts}\n'
   end
